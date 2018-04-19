bf80410b1d7fb74aa9b2faece6deb9280710a277
neil.middleton <neil.middleton@thomsonreuters.com>
2018-04-19 13:44:53 +0100

    paths

---
+++ TimerTriggerJS1/index.js
@@ -0,0 +1,26 @@
+const rp = require('./lib/request-promise');
+module.exports = function (context, myTimer) {
+    var timeStamp = new Date().toISOString();
+
+    if(myTimer.isPastDue)
+    {
+        context.log('JavaScript is running late!');
+    }
+    const getToken = function(resource, apiver, context) {
+        var options = {
+            uri: `${process.env["MSI_ENDPOINT"]}/?resource=${resource}&api-version=${apiver}`,
+            headers: {
+                'Secret': process.env["MSI_SECRET"]
+            }
+        };
+        rp(options)
+            .then(function(result){
+                context.log('RPRETURNED', JSON.stringify(result));
+            });
+    };
+    getToken('https://vault.azure.net', '2017-09-01', context);
+    context.log('JavaScript timer trigger function ran!:', timeStamp);
+
+
+    context.done();
+};
